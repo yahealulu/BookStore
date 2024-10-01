44af602b5754145f3a61b788244cfc19acc2a7f8
yahealulu <yahea.luoluo@gmail.com>
2024-10-01 17:56:27 +0300

    editing project

--- src/components/AutoSlider/AutoSlider.jsx
+++ src/components/AutoSlider/AutoSlider.jsx
@@ -7,9 +7,9 @@ export const AutoSlider = ({ images, duration }) => {
                 <div className='flex gap-5 cursor-pointer animate-swipe hover:[animation-play-state:paused]' style={{animationDuration : `${duration}s`}}>
 
                     { 
-                        [...images, ...images, ...images].map(({ src, title, author }) => (
-                            <div className='flex flex-col items-start flex-shrink-0'>
-                                <Image src={src} alt={'Nuclear War'} width={195} height={195} className='w-52 h-52 object-cover rounded-md' />
+                        [...images, ...images, ...images].map(({ src, title, author },index) => (
+                            <div key={index} className='flex flex-col items-start flex-shrink-0'>
+                                <Image src={src} alt={'Nuclear War'}  width={195} height={195} className='w-52 h-52 object-cover rounded-md' />
                                 <span className='pt-2 font-bold'>{title}</span>
                                 <span className='text-gray-600 dark:text-gray-300'>{author}</span>
                             </div>
